ef9a80f6a83091c83ab35534a015cab6feb18270
Midnight Doggo <git@midnightdoggo19.com>
2025-03-03 22:54:51 +0000

    bonk

---
+++ commands/utility/bonk.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('bonk')
+		.setDescription('bonk')
+        .addUserOption(option =>
+            option.setName('user')
+            .setDescription('User to bonk')
+            .setRequired(true)
+        )
+        .setContexts(0, 1, 2)
+        .setIntegrationTypes(0, 1),
+
+	async execute(interaction) {
+		await interaction.deferReply();
+
+        const user = interaction.options.getUser('user');
+        if (!user) return interaction.edirReply('Could not find the user.');
+
+        const embed = new EmbedBuilder()
+            .setTitle('Boop!')
+            .setDescription(`<@${interaction.user.id}> bonked <@${user.id}>!`)
+            .setImage('https://media1.tenor.com/m/udpvUVcjpZAAAAAC/bonk.gif')
+            .setTimestamp();
+
+        await interaction.editReply({ embeds: [embed] });
+	},
+};
