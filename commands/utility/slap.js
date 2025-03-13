dc5f7d7083537a19c1bb70635e642c529574b806
Midnight Doggo <git@midnightdoggo19.com>
2025-03-13 19:40:05 +0000

    slap somebody

---
+++ commands/utility/slap.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { logger } = require('../../functions');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('slap')
+		.setDescription('slap somebody')
+        .addUserOption(option =>
+            option.setName('user')
+            .setDescription('User to slap')
+            .setRequired(true)
+        )
+        .setContexts(0, 1, 2)
+        .setIntegrationTypes(0, 1),
+
+	async execute(interaction) {
+		await interaction.deferReply();
+
+        const user = interaction.options.getUser('user');
+        if (!user) return interaction.edirReply('Could not find the user.');
+
+        const embed = new EmbedBuilder()
+            .setTitle('high quality title © 2025 Midnight Doggo')
+            .setDescription(`<@${interaction.user.id}> slapped <@${user.id}>!`)
+            .setImage('https://midnightdoggo19.com/assets/boop/slap_optimized.gif')
+            .setTimestamp();
+
+        await interaction.editReply({ embeds: [embed] });
+        logger.debug(`${interaction.user.id} slapped ${user.id}`);
+	},
+};
