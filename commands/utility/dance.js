7cfc7e9a13d92a8385a2dc0f8bcebf3ed35f8f1c
Midnight Doggo <git@midnightdoggo19.com>
2025-05-20 17:16:28 +0100

    dancing

---
+++ commands/utility/dance.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { logger } = require('../../functions');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('dance')
+        .setDescription('dance the night away')
+        .addUserOption(option =>
+            option.setName('user')
+            .setDescription('user to dance at')
+            .setRequired(true)
+        )
+        .setContexts(0, 1, 2)
+        .setIntegrationTypes(0, 1),
+
+    async execute(interaction) {
+        await interaction.deferReply();
+
+        const user = interaction.options.getUser('user');
+        if (!user) return interaction.editReply('Could not find the user.');
+
+        const embed = new EmbedBuilder()
+            .setTitle('boogie robots')
+            .setDescription(`<@${interaction.user.id}> is dancing at <@${user.id}>!`)
+            .setImage('https://midnightdoggo19.com/assets/boop/dance_optimized.gif')
+            .setTimestamp();
+
+        await interaction.editReply({ embeds: [embed] });
+        logger.debug(`${interaction.user.id} dance at ${user.id}`);
+    },
+};
